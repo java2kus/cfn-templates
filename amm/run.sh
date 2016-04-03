1b8513f874e9ed5e35e26a7013757e28798d585b
russomi <russo.mail@gmail.com>
2016-04-03 18:58:13 -0400

    moves

--- amm/run.sh
+++ amm/run.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 aws cloudformation create-stack --stack-name $1 --template-body file://amm-master.cfn.json --parameters file://launch-params.json --capabilities CAPABILITY_IAM --disable-rollback
